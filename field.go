c31d69d3370807c1fd67ff6ee9695d721c708dc9
Daniel Cox <danzabian@gmail.com>
2015-10-29 18:08:45 +0000

    Added support for extra field types, adding some basic string validation to fields

--- field.go
+++ field.go
@@ -7,16 +7,23 @@ import (
 type Field struct {
 	// The field name
 	Name string
+	// The field label
+	Label string
 	// The field type ie text, textarea
 	Type string
 	// The default value
 	Default string
 	// The actual value
 	Value string
+	// A list of values to use for multi-value inputs
+	Values []string
+	// A list of validation items to add to this field
+	Rules []string
 	// A list of errors on this field
 	Errors []string
 }
 
+// Creates a new field struct
 func NewField() *Field {
 	return &Field{
 		Name: "Field",
@@ -24,23 +31,51 @@ func NewField() *Field {
 	}
 }
 
+// Returns the string representation of a field, depending on its type
 func (f Field) output() string {
 	switch f.Type {
 	case "textarea":
 		return f.outputTextArea()
+	case "radio":
+	case "checkbox":
+		return f.outputBox()
 	}
 
 	return f.outputText()
 }
 
+// Adds an error to the field
 func (f *Field) addError(err string) {
 	f.Errors = append(f.Errors, err)
 }
 
+// Returns the label for a field
+func (f Field) label() string {
+	return fmt.Sprintf("<label for=\"%s\">%s</label>", f.Name, f.Label)
+}
+
+// Returns the string representation of a standard text input
 func (f Field) outputText() string {
 	return fmt.Sprintf("<input type=\"%[1]s\" name=\"%[2]s\" id=\"%[2]s\" value=\"%[3]s\" />", f.Type, f.Name, f.Value)
 }
 
+// Returns the string representation of a textarea input
 func (f Field) outputTextArea() string {
 	return fmt.Sprintf("<textarea name=\"%[1]s\" id=\"%[1]s\">%[2]s</textarea>", f.Name, f.Value)
+}
+
+// Returns the string representation of a checkbox/radio
+// Currently, this is exactly the same as outputText, however there
+// Will be some difference once request form values are added
+func (f Field) outputBox() string {
+	return fmt.Sprintf("<input type=\"%[1]s\" name=\"%[2]s\" id=\"%[2]s\" value=\"%[3]s\" />", f.Type, f.Name, f.Value)
+}
+
+// Checks the field value to make sure something is set
+func (f *Field) validateRequired() bool {
+	if len(f.Value) == 0 {
+		f.addError("This field is required")
+		return false
+	}
+	return true
 }
--- field_test.go
+++ field_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 )
 
+// Testing the output of a text field
 func TestFieldOutputText(t *testing.T) {
 	field := NewField()
 	field.Name = "foo"
@@ -15,6 +16,18 @@ func TestFieldOutputText(t *testing.T) {
 	assert.Equal(t, "<input type=\"text\" name=\"foo\" id=\"foo\" value=\"\" />", tag)
 }
 
+// Testing the output of a checkbox/radio
+func TestFieldOutputBox(t *testing.T) {
+	field := NewField()
+	field.Name = "foo"
+	field.Type = "checkbox"
+
+	tag := field.output()
+
+	assert.Equal(t, "<input type=\"checkbox\" name=\"foo\" id=\"foo\" value=\"\" />", tag)
+}
+
+// Testing the output of a textarea field
 func TestFieldOutputTextArea(t *testing.T) {
 	field := NewField()
 	field.Name = "foo"
@@ -25,9 +38,32 @@ func TestFieldOutputTextArea(t *testing.T) {
 	assert.Equal(t, "<textarea name=\"foo\" id=\"foo\"></textarea>", tag)
 }
 
+// Testing the output of a label
+func TestFieldOutputLabel(t *testing.T) {
+	field := NewField()
+	field.Name = "foo"
+	field.Label = "Foo"
+
+	assert.Equal(t, "<label for=\"foo\">Foo</label>", field.label())
+}
+
+// Testing the add error method
 func TestFieldAddError(t *testing.T) {
 	field := NewField()
 	field.addError("A test error")
 
+	assert.Equal(t, 1, len(field.Errors))
+}
+
+// Testing the required validation rule
+func TestFieldValidationRequired(t *testing.T) {
+	field := NewField()
+	field.Value = "test"
+
+	assert.Equal(t, true, field.validateRequired())
+
+	field.Value = ""
+
+	assert.Equal(t, false, field.validateRequired())
 	assert.Equal(t, 1, len(field.Errors))
 }
