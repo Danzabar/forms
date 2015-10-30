afd5ff0111a0df159ddb1f80c4ab75f6a94add43
Daniel Cox <danzabian@gmail.com>
2015-10-30 07:32:41 +0000

    Adding length validation and tests

--- validation.go
+++ validation.go
@@ -11,10 +11,24 @@ type Required struct {
 	Valid bool
 }
 
+// Checks for a specific length
+type LengthValidation struct {
+	Value string
+	Err   string
+	Valid bool
+	Len   int
+}
+
 func (r *Required) validate() {
 	// We only follow the happy path here because go sets Valid as
 	// its null value (false)
 	if len(r.Value) > 0 {
 		r.Valid = true
+	}
+}
+
+func (l *LengthValidation) validate() {
+	if len(l.Value) == l.Len {
+		l.Valid = true
 	}
 }
--- validation_test.go
+++ validation_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 )
 
+// Tests the required validation
 func TestRequiredValidation(t *testing.T) {
 	req := &Required{
 		Err: "This field is required",
@@ -13,4 +14,27 @@ func TestRequiredValidation(t *testing.T) {
 	req.validate()
 
 	assert.Equal(t, false, req.Valid)
+
+	req.Value = "test"
+	req.validate()
+
+	assert.Equal(t, true, req.Valid)
+}
+
+// Tests the length validation
+func TestLengthValidation(t *testing.T) {
+	val := &LengthValidation{
+		Err: "This field should be 5 characters in length",
+		Len: 5,
+	}
+
+	val.Value = "fail"
+	val.validate()
+
+	assert.Equal(t, false, val.Valid)
+
+	val.Value = "tests"
+	val.validate()
+
+	assert.Equal(t, true, val.Valid)
 }
