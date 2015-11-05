7b89ef7f9a0ecbbb1a03e8549ca235aea716cad2
Daniel Cox <danzabian@gmail.com>
2015-11-05 19:47:30 +0000

    updating yaml form builder

--- builder.go
+++ builder.go
@@ -3,6 +3,7 @@ package forms
 import (
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
+	"net/http"
 	"path/filepath"
 )
 
@@ -14,28 +15,44 @@ type Builder struct {
 	Config Config
 	// The form struct that will be populated from the yaml file
 	Form *Form
+	// Instance of the http request
+	Request http.Request
 }
 
 // The YAML config struct
 type Config struct {
-	Name        string `yaml:"Name"`
-	Description string `yaml:"Description"`
-	Action      string `yaml:"Action"`
-	Method      string `yaml:"Method"`
+	Name        string        `yaml:"Name"`
+	Description string        `yaml:"Description"`
+	Action      string        `yaml:"Action"`
+	Method      string        `yaml:"Method"`
+	Fields      []ConfigField `yaml:"Fields"`
 }
 
-func NewBuilder(file string) *Builder {
+// The YAML config struct for a field
+type ConfigField struct {
+	Name    string   `yaml:"Name"`
+	Label   string   `yaml:"Label"`
+	Type    string   `yaml:"Type"`
+	Default string   `yaml:"Default"`
+	Values  []string `yaml:"Values"`
+	Rules   []string `yaml:"Rules"`
+}
+
+// Creates a new form builder and runs the extract method
+func NewBuilder(file string, r http.Request) *Builder {
 	path, _ := filepath.Abs(file)
 
 	b := &Builder{
 		Filename: path,
+		Request:  r,
 	}
-	// Extract settings
+
 	b.extract()
 
 	return b
 }
 
+// Extracts values from the YAML file and builds a config struct
 func (b *Builder) extract() {
 	data, _ := ioutil.ReadFile(b.Filename)
 
@@ -44,4 +61,27 @@ func (b *Builder) extract() {
 	yaml.Unmarshal([]byte(data), &c)
 
 	b.Config = c
+}
+
+// Creates the form object from the configuration
+func (b *Builder) build() {
+	// Create the form struct
+	b.Form = NewForm(b.Request)
+	b.Form.Name = b.Config.Name
+	b.Form.Description = b.Config.Description
+	b.Form.Action = b.Config.Action
+	b.Form.Method = b.Config.Method
+
+	// Now we can add fields
+	for _, field := range b.Config.Fields {
+		f := &Field{
+			Name:    field.Name,
+			Label:   field.Label,
+			Type:    field.Type,
+			Default: field.Default,
+			Values:  field.Values,
+		}
+
+		b.Form.addField(f)
+	}
 }
--- builder_test.go
+++ builder_test.go
@@ -2,11 +2,40 @@ package forms
 
 import (
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+	"net/http"
 	"testing"
 )
 
-func TestParseYamlFile(t *testing.T) {
-	b := NewBuilder("example.yml")
+// Test suite
+type BuilderTestSuite struct {
+	suite.Suite
+	Req http.Request
+}
 
-	assert.Equal(t, "Test", b.Config.Name)
+// Test setup method
+func (suite *BuilderTestSuite) SetupTest() {
+	suite.Req = http.Request{
+		Method: "POST",
+	}
+}
+
+// Testing basic parsing of YAML form
+func (suite *BuilderTestSuite) TestParseYamlFile() {
+	b := NewBuilder("example.yml", suite.Req)
+
+	assert.Equal(suite.T(), "Test", b.Config.Name)
+}
+
+// Test building a usable form from the yaml file
+func (suite *BuilderTestSuite) TestBuildingForm() {
+	b := NewBuilder("example.yml", suite.Req)
+
+	b.build()
+	assert.Equal(suite.T(), 2, len(b.Form.Fields))
+}
+
+// Test runner
+func BuilderTestRunnerTestSuite(t *testing.T) {
+	suite.Run(t, new(BuilderTestSuite))
 }
