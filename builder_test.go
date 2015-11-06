2026175b2f1a26be91179cab21468f28d570111c
Daniel Cox <danzabian@gmail.com>
2015-11-06 06:29:47 +0000

    Added test to ensure values are still set on request

--- builder_test.go
+++ builder_test.go
@@ -2,7 +2,9 @@ package forms
 
 import (
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"net/http"
+	"strings"
 	"testing"
 )
 
@@ -21,6 +23,31 @@ func TestBuildingForm(t *testing.T) {
 
 	b.build()
 	assert.Equal(t, 4, len(b.Form.Fields))
+}
+
+// Test building a form with pre-defined values
+func TestBuildPreValues(t *testing.T) {
+	r := http.Request{
+		Method: "POST",
+		Body:   ioutil.NopCloser(strings.NewReader("test=foo&test2=bar&test3=test1")),
+		Header: http.Header{
+			"Content-Type": {"application/x-www-form-urlencoded"},
+		},
+	}
+
+	b := NewBuilder("example.yml", r)
+	b.build()
+
+	for _, field := range b.Form.Fields {
+		switch field.Name {
+		case "test":
+			assert.Equal(t, "foo", field.Value)
+		case "test2":
+			assert.Equal(t, "bar", field.Value)
+		case "test3":
+			assert.Equal(t, "test1", field.Value)
+		}
+	}
 }
 
 // Build benchmark
